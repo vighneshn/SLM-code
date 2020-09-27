abf7908424de8b8eb5bd2e394396ba25e25ae79d
Vighnesh Natarajan <vighnesh@sirius.local>
2020-09-27 22:46:59 +0530

    Updates combine_slm_grab

--- combine_slm_grab.py
+++ combine_slm_grab.py
@@ -56,29 +56,45 @@ print("dataHeight = " + str(dataHeight))
 
 # Calculate the data:
 
+##CHange Threshold
+Error = 999999999999999999
+Threshold = 1
+a = 0.5
+def error(I_target, I_camera):
+    return np.sum((abs(I_target-I_camera))**2)/np.size(I_target)
+
+##Set this appropriately, can load the .npy file into this, and resize probably
+TargetIntensity = np.ones((grabResult.Height, grabResult.Width))
+
 numberOfImagesToGrab = 200
 camera.StartGrabbingMax(numberOfImagesToGrab)
 time_start = time.time()
-while camera.IsGrabbing():
+while camera.IsGrabbing() and (Error>THRESHOLD):
     error = slm.showData(data)
     grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
 
     if grabResult.GrabSucceeded():
         # Access the image data.
-        img = np.asarray(grabResult.Array)#.reshape((grabResult.Height, grabResult.Width))
-        img[img<0.01] = 0
+        img = (inp.asarray(grabResult.Array))**0.5 #.reshape((grabResult.Height, grabResult.Width))
+        Error = error(TargetIntensity, img**2)
 
         ###ADDING FFT in Comment
         #img = np.fft.fftshift(np.fft.fft2(img))
 
+        img_fb = np.fft.fftshift(np.fft.ifft2(a*img+(1-a)*(TargetIntensity)**0.5))
+        img_fb_phase = np.degrees(img_fb)*np.pi/180
+
         print(img.shape)
-        for y in range(dataHeight):
-            for x in range(dataWidth):
-                data[y,x] = int(img[int(x/dataHeight*grabResult.Height), int(y/dataWidth*grabResult.Width)])
+
+        data = img_fb_phase
+
+        #for y in range(dataHeight):
+        #    for x in range(dataWidth):
+        #        data[y,x] = int(img[int(x/dataHeight*grabResult.Height), int(y/dataWidth*grabResult.Width)])
+
 
     time2 = time.time()
     print("TIME: ", time2-time_start)
-
     ##Wait statements
     #time.sleep(5)
     #input("Press Enter to continue...")
@@ -86,18 +102,10 @@ while camera.IsGrabbing():
     grabResult.Release()
 camera.Close()
 
-
-
-# Show data on SLM:
-
-
-assert error == ErrorCode.NoError, slm.errorString(error)
-
 # If your IDE terminates the python interpreter process after the script is finished, the SLM content
 # will be lost as soon as the script finishes.
 # You may insert further code here.
 # Wait until the SLM process is closed:
-
 error = slm.utilsWaitUntilClosed()
 assert error == ErrorCode.NoError, slm.errorString(error)
 # Unloading the SDK may or may not be required depending on your IDE:
